766c0421a53bd25a273132d1df479ad3fd36cdb8
Maxlego08 <maxlego08@sfr.fr>
2024-03-31 21:08:36 +0200

    :tada: First commit !

---
+++ Essentials/src/main/java/fr/maxlego08/essentials/ZEssentialsPluginPlugin.java
@@ -0,0 +1,18 @@
+package fr.maxlego08.essentials;
+
+import fr.maxlego08.essentials.api.EssentialsPlugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZEssentialsPluginPlugin extends JavaPlugin implements EssentialsPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
