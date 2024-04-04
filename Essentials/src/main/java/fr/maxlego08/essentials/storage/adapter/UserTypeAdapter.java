73d2cc2d7b1725f850c4555523efece6fa68b54c
Maxlego08 <maxlego08@sfr.fr>
2024-04-04 20:39:30 +0200

    :memo: FIxs

--- Essentials/src/main/java/fr/maxlego08/essentials/storage/adapter/UserTypeAdapter.java
+++ Essentials/src/main/java/fr/maxlego08/essentials/storage/adapter/UserTypeAdapter.java
@@ -4,14 +4,18 @@ import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import fr.maxlego08.essentials.api.EssentialsPlugin;
+import fr.maxlego08.essentials.api.database.dto.CooldownDTO;
+import fr.maxlego08.essentials.api.database.dto.OptionDTO;
 import fr.maxlego08.essentials.api.user.Option;
 import fr.maxlego08.essentials.api.user.User;
 import fr.maxlego08.essentials.user.ZUser;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class UserTypeAdapter extends TypeAdapter<User> {
 
@@ -54,21 +58,28 @@ public class UserTypeAdapter extends TypeAdapter<User> {
     @Override
     public User read(JsonReader in) throws IOException {
         String name = null;
-        User user = new ZUser(this.plugin, UUID.randomUUID());
+        UUID uniqueId = null; // Temporary storage for the UUID
+        Map<Option, Boolean> options = new HashMap<>();
+        Map<String, Long> cooldowns = new HashMap<>();
+        Map<String, BigDecimal> balances = new HashMap<>();
 
         in.beginObject();
         while (in.hasNext()) {
             switch (in.nextName()) {
-                case "uniqueId" -> new ZUser(this.plugin, UUID.fromString(in.nextString()));
+                case "uniqueId" -> uniqueId = UUID.fromString(in.nextString());
                 case "name" -> name = in.nextString();
                 case "options" -> {
                     in.beginObject();
-                    while (in.hasNext()) user.setOption(Option.valueOf(in.nextName()), in.nextBoolean());
+                    while (in.hasNext()) {
+                        options.put(Option.valueOf(in.nextName()), in.nextBoolean());
+                    }
                     in.endObject();
                 }
                 case "cooldowns" -> {
                     in.beginObject();
-                    while (in.hasNext()) user.setCooldown(in.nextName(), in.nextLong());
+                    while (in.hasNext()) {
+                        cooldowns.put(in.nextName(), in.nextLong());
+                    }
                     in.endObject();
                 }
                 case "balances" -> {
@@ -76,16 +87,27 @@ public class UserTypeAdapter extends TypeAdapter<User> {
                     while (in.hasNext()) {
                         String key = in.nextName();
                         BigDecimal value = new BigDecimal(in.nextString());
-                        user.setBalance(key, value);
+                        balances.put(key, value);
                     }
                     in.endObject();
                 }
-
             }
         }
         in.endObject();
 
+        // Ensure that uniqueId is not null before creating a ZUser
+        if (uniqueId == null) {
+            throw new IOException("UniqueId is missing from the JSON input.");
+        }
+        User user = new ZUser(this.plugin, uniqueId); // Create the ZUser here
+
+        // Now, set the other properties
         user.setName(name);
+        user.setOptions(options.entrySet().stream().map(e -> new OptionDTO(e.getKey(), e.getValue())).collect(Collectors.toList()));
+        user.setCooldowns(cooldowns.entrySet().stream().map(e -> new CooldownDTO(e.getKey(), e.getValue())).collect(Collectors.toList()));
+        balances.forEach(user::setBalance);
+
         return user;
     }
+
 }
