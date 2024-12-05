e3c1b339f4576b9c52944d3f706e55103fe4599d
sinoncute <caothanhhien62@gmail.com>
2024-12-05 11:18:19 -0800

    fix make config kit display type work as intended

--- src/main/java/fr/maxlego08/essentials/kit/KitModule.java
+++ src/main/java/fr/maxlego08/essentials/kit/KitModule.java
@@ -37,7 +37,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class KitModule extends ZModule {
 
     private final List<Kit> kits = new ArrayList<>();
-    private final KitDisplay display = KitDisplay.IN_LINE;
+    private KitDisplay display = KitDisplay.IN_LINE;
 
     public KitModule(ZEssentialsPlugin plugin) {
         super(plugin, "kits");
@@ -47,6 +47,8 @@ public class KitModule extends ZModule {
     @Override
     public void loadConfiguration() {
         super.loadConfiguration();
+
+        this.display = KitDisplay.valueOf(getConfiguration().getString("display", KitDisplay.IN_LINE.name()));
 
         this.loadKits();
 
@@ -147,7 +149,7 @@ public class KitModule extends ZModule {
     }
 
     public List<Kit> getKits(Permissible permissible) {
-        return this.kits.stream().filter(kit -> permissible.hasPermission(Permission.ESSENTIALS_KIT.asPermission(kit.getName()))).toList();
+        return this.kits.stream().filter(kit -> permissible.hasPermission(Permission.ESSENTIALS_KIT_.asPermission(kit.getName()))).toList();
     }
 
     public boolean giveKit(User user, Kit kit, boolean bypassCooldown) {
