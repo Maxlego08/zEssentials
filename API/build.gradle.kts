1ba366cd1c4096de97c818f490ea3bbdc92a3caf
Maxlego08 <maxlego08@sfr.fr>
2025-06-08 16:02:49 +0200

    :memo: fix publish

--- API/build.gradle.kts
+++ API/build.gradle.kts
@@ -32,7 +32,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/${System.getenv("GITHUB_REPOSITORY_OWNER")}/zMenu")
+            url = uri("https://maven.pkg.github.com/${System.getenv("GITHUB_REPOSITORY_OWNER")}/zEssentials")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
