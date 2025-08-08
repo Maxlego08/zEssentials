6e4179e00e6a5abc64914acd6d51e28fbb595ed2
Maxlego08 <maxlego08@sfr.fr>
2025-08-08 21:31:26 +0200

    :memo: change protocolblis

--- Hooks/ProtocolLib/build.gradle.kts
+++ Hooks/ProtocolLib/build.gradle.kts
@@ -1,11 +1,7 @@
 group = "Hooks:ProtocolLib"
 
-repositories {
-    maven(url = "https://repo.dmulloy2.net/repository/public/")
-}
-
 dependencies {
     compileOnly(project(":API"))
-    compileOnly("com.comphenix.protocol:ProtocolLib:5.0.0")
+    compileOnly("net.dmulloy2:ProtocolLib:5.4.0")
     compileOnly("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
 }
