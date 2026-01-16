c8de29d1126a4e93a9844aa84e788ebc0ef469b6
Maxence <simon.maxence@hotmail.com>
2026-01-16 13:38:08 +0100

    :memo: fixs

--- NMS/V1_21_10/build.gradle.kts
+++ NMS/V1_21_10/build.gradle.kts
@@ -6,6 +6,7 @@ group "NMS:V1_21_10"
 
 dependencies {
     compileOnly(project(":API"))
+    compileOnly("net.kyori:adventure-api:4.26.1")
     paperweight.paperDevBundle("1.21.10-R0.1-SNAPSHOT")
 }
 
